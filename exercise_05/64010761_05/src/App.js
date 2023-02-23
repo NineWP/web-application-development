8c840ce70eee700af48b68b6c4229b1f117df298
Worrapon <nine6805@gmail.com>
2023-02-23 15:20:10 +0700

    start react exercise 05

---
+++ exercise_05/64010761_05/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MenuCard from './components/MenuCard';
+
+function App() {
+    return (
+        <div className="container">
+            <header>โหวตอาหาร</header>
+            <MenuCard/>
+        </div> 
+    );
+}
+
+export default App;
---
+++ exercise_05/64010761_05/src/components/MenuData.js
@@ -0,0 +1,16 @@
+export const MenuData = [
+    {
+        type: "อาหารคาว",
+        name: "ข้าวผัด",
+        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veniam maiores, exercitationem molestias harum debitis illum reiciendis explicabo voluptas iusto sapiente inventore totam suscipit. Corporis beatae hic dolore odio voluptatum.",
+        img_src: "https://www.ajinomoto.co.th//storage/photos/shares/our-story/tips/friedrice/62ff47ff5a301.jpg",
+        alt: "ข้าวผัด_pic"
+    },
+    {
+        type: "อาหารหวาน",
+        name: "บัวลอย",
+        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veniam maiores, exercitationem molestias harum debitis illum reiciendis explicabo voluptas iusto sapiente inventore totam suscipit. Corporis beatae hic dolore odio voluptatum.",
+        img_src: "http://lamphuonline.com/images/00-2022/bua-loi-kaiwan.jpg",
+        alt: "บัวลอย_pic"
+    }
+]
