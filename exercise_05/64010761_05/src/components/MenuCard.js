1c8c9719db2e419e84dca865af9e0494a5f98b82
Worrapon <nine6805@gmail.com>
2023-02-23 16:01:10 +0700

    react exercise 05 add click to vote

--- exercise_05/64010761_05/src/components/MenuCard.js
+++ exercise_05/64010761_05/src/components/MenuCard.js
@@ -8,12 +8,19 @@ const MenuCard = () => {
         {MenuData.map((item, index) => {
             return (
                 <div key={index} className="card">
-                    <div className="info">
-                        <h3>{item.type}</h3>
-                        <h4>{item.name}</h4>
-                        <p>{item.title}</p>
+                    <div className="main-content">
+                        <div className="info">
+                            <h3>{item.type}</h3>
+                            <h4>{item.name}</h4>
+                            <p>{item.title}</p>
+                        </div>
+                        <img className="menu-img" src={item.img_src} alt={item.alt}/>
                     </div>
-                    <img className="menu-img" src={item.img_src} alt={item.alt}/>
+                    <footer>
+                        <button className='vote'>Click to Vote</button>
+                        <span className='display-count'>MIN</span>
+                        <button className='unvote'>Click to Unvote</button>
+                    </footer>
                 </div>
             )
         })}
--- exercise_05/64010761_05/src/components/MenuData.js
+++ exercise_05/64010761_05/src/components/MenuData.js
@@ -4,13 +4,15 @@ export const MenuData = [
         name: "ข้าวผัด",
         title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veniam maiores, exercitationem molestias harum debitis illum reiciendis explicabo voluptas iusto sapiente inventore totam suscipit. Corporis beatae hic dolore odio voluptatum.",
         img_src: "https://www.ajinomoto.co.th//storage/photos/shares/our-story/tips/friedrice/62ff47ff5a301.jpg",
-        alt: "ข้าวผัด_pic"
+        alt: "ข้าวผัด_pic",
+        count: 0
     },
     {
         type: "อาหารหวาน",
         name: "บัวลอย",
         title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veniam maiores, exercitationem molestias harum debitis illum reiciendis explicabo voluptas iusto sapiente inventore totam suscipit. Corporis beatae hic dolore odio voluptatum.",
         img_src: "http://lamphuonline.com/images/00-2022/bua-loi-kaiwan.jpg",
-        alt: "บัวลอย_pic"
+        alt: "บัวลอย_pic",
+        count: 0
     }
 ]
